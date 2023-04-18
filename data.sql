1a645b54b8953d3497209a04e51b59f2d2a65456
Lugard Agu <agulugard@gmail.com>
2023-04-18 18:07:39 +0100

    Add data to database

--- data.sql
+++ data.sql
@@ -86,7 +86,6 @@ INSERT INTO visits VALUES
 (10, 3, '2020-05-24'),
 (10, 1, '2021-01-11');
 
-
 -- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
 INSERT INTO visits (animals_id, vets_id, date_of_visits) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 
